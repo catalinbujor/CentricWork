1366c5432a1989ce37f198907907184f9b2bbe31
Unknown <mihai.bujor@info.uaic.ro>
2018-07-05 17:25:12 +0300

    Angular first commit

---
+++ Angular tutorial/myapp/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {headerComponent} from './header/app.headerComponent';
+import {navComponent} from './nav/app.navComponent';
+import {footerComponent} from './footer/app.footerComponent';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './main.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
---
+++ Angular tutorial/myapp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import {headerComponent} from './header/app.headerComponent';
+import {navComponent} from './nav/app.navComponent';
+import {contentAreaComponent} from './contentArea/app.contentAreaComponent';
+import {footerComponent} from './footer/app.footerComponent';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,headerComponent,navComponent,contentAreaComponent,footerComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
